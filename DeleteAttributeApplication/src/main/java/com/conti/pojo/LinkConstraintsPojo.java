e1e4f65a42f66669f1114b02e6836bef796587a0
nidishanand <45270969+nidishanand@users.noreply.github.com>
2024-05-08 02:00:16 +0530

    Add files via upload

---
+++ DeleteAttributeApplication/src/main/java/com/conti/pojo/LinkConstraintsPojo.java
@@ -0,0 +1,30 @@
+package com.conti.pojo;
+
+public class LinkConstraintsPojo {
+	
+	private String sourceLink;
+	private String targetLink;
+	private String linkType;
+	
+	public String getSourceLink() {
+		return sourceLink;
+	}
+	public void setSourceLink(String sourceLink) {
+		this.sourceLink = sourceLink;
+	}
+	public String getTargetLink() {
+		return targetLink;
+	}
+	public void setTargetLink(String targetLink) {
+		this.targetLink = targetLink;
+	}
+	public String getLinkType() {
+		return linkType;
+	}
+	public void setLinkType(String linkType) {
+		this.linkType = linkType;
+	}
+	
+	
+
+}
